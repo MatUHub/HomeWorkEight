d952add19347874a0049f2f2b26a887404910478
abricos <fruts@nadereve.ru>
2021-08-16 19:45:05 +0300

    создание проекта

---
+++ app/src/main/java/com/example/homeworkeight/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.homeworkeight;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().
+                    beginTransaction().
+                    replace(R.id.notes_container, NotesFragment.newInstance()).
+                    commit();
+            }
+        }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Fragment backStackFragment = (Fragment)getSupportFragmentManager().findFragmentById(R.id.notes_container);
+
+        if(backStackFragment != null && backStackFragment instanceof FragmentDescription){
+            onBackPressed();
+        }
+    }
+}
+
